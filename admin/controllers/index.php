e9fcd4efa416ea5d4fbc38e447c4e83114c7414d
Lusifer97 <deshan.bandara1997@gmail.com>
2022-07-11 04:19:40 +0530

    #35 Route correction admin panel

--- admin/controllers/index.php
+++ admin/controllers/index.php
@@ -10,32 +10,16 @@ class Index extends Controller
     }
 
     function index()
-    {
-         
-        // $this->load_js(
-        //     [
-                
-        //         ]
-        // );
-        // echo Cookie::set('jjjj', 'aaaaaaaa', time() + (86400 * 30), '/');
-        //    echo Cookie::unset_cookie('name2','/');
-        // var_dump($_COOKIE);
-        // $this->view->topDestinations = $this->model->popularDestinations();
-        $this->view->allPackages = $this->model->allPackages();
-        // $this->view->allHotels = $this->model->allHotels();
-        // $this->view->feedbacks = $this->model->feedbacks();
-        // $this->view->blog = $this->model->blog();
-        // $this->view->count = $this->model->destinationCount();
-        $this->view->title = "Home";
+    { 
+        $this->view->title = "Admin";
         $this->view->render('header');
+        // $this->view->render('navigation');
         $this->view->render('index/index');
         $this->view->render('footer');
-        // var_dump(LOGIN_CONFIG);
-        //   var_dump( Hash::create('whirlpool', '1234', SALT));
-
         
     }
-   
+     
+    // SAMPLES
 
 
 }
--- admin/libs/View.php
+++ admin/libs/View.php
@@ -10,6 +10,5 @@ class View {
     public function render($name) {
         require 'views/' . $name . '.php';
     }
-    
-
+   
 }
